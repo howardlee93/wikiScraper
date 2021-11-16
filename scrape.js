99a9bcfe9401e984ba74740ef9dea09cf441b341
howard lee <howardlee@howards-MacBook-Pro.local>
2021-11-16 15:11:18 -0800

    adding server

---
+++ scrape.js
@@ -0,0 +1,68 @@
+const puppeteer = require('puppeteer');
+const readline = require('readline');
+var fs = require('fs');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What is the term you want to scrape ', (searchTerm) => {
+    search(searchTerm);
+    rl.close();
+});
+
+
+const search = async (term) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    // await page.goto('https://en.wikipedia.org/wiki/Operation_Grapple', {waitUntil : 'domcontentloaded'});
+    // await page.goto('https://en.wikipedia.org/wiki/Cimoliopterus', {waitUntil : 'domcontentloaded'});
+
+    await page.goto(`https://en.wikipedia.org/wiki/${term}`, {waitUntil : 'domcontentloaded'});
+
+    const imageBox = await page.$eval('#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(2) > td > a > img',
+        el =>  el.getAttribute('src')
+    );
+
+    const info =  await page.$$eval('#mw-content-text > div.mw-parser-output > table.infobox > tbody tr', table => {
+        // let tableRow =[];
+        // for (let i = 1; i <table.length; i++){
+        //     tableRow.push(table[i]);
+        // }
+        // return tableRow;
+
+        return table[2].innerText;
+    });
+
+    console.log("image", imageBox);
+
+    console.log("info", info);
+
+   await page.pdf({ path: `res/${term}.pdf`, format: 'a4' });
+
+    await browser.close();
+};
+
+
+const htmlBuilder = ()=>{
+    var header = '';
+    var body = '';
+
+}
+
+
+// const main = async () =>{
+//     rl.question('What is the term you want to scrape ', (searchTerm) => {
+//         // TODO: Log the answer in a database
+//         console.log(`you are searching for : ${searchTerm}`);
+//         // return searchTerm;
+//         search(searchTerm);
+//         rl.close();
+//     });
+
+// }
+
+// main();
+
+
