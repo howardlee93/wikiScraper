6cef9e99fe0685d49f9ed6720945feaf71dd5086
howard lee <howardlee@howards-MacBook-Pro.local>
2022-01-28 11:06:51 -0800

    search function

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,20 +1,24 @@
 <script>
 	export let searchTerm;
 	let baseURL = 'http://localhost:5000/api/';
+
+
+	function handleSubmit(){
+		console.log(searchTerm);
+	}
 	
-	fetch(url,{
-		method:'POST'
-	});
+	// fetch(url,{
+	// 	method:'POST'
+	// });
 
 
 </script>
 
 <main>
 	<h1>Wikipedia search wrapper</h1>
-	<input bind:value={$searchTerm} />
+	<input bind:value={searchTerm} on:change={handleSubmit}/>
 	<p></p>
 	<img src="//upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/260px-Cat_poster_1.jpg" alt="wiki-image"/>
-	<iframe src="../../res/cat.pdf"></iframe>
 </main>
 
 <style>
